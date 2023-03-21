5953659022e2f4083495e959b374c16432e78068
imam-syafi <122346179+imam-syafi@users.noreply.github.com>
2023-03-21 13:14:10 +0700

    Compare two dices

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Unit 1 Pathway 4 of Android Basics in Kotlin
 
 ## Demo
 
-[device-2023-03-21-132214.webm](https://user-images.githubusercontent.com/122346179/226532014-76bdb917-5db1-4d88-807b-08279bd3dd60.webm)
+[device-2023-03-21-132422.webm](https://user-images.githubusercontent.com/122346179/226532283-47de4949-3370-405f-9675-16db6a2df508.webm)
--- app/src/main/java/com/edts/diceroller/MainActivity.kt
+++ app/src/main/java/com/edts/diceroller/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.edts.diceroller
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isInvisible
 import androidx.core.view.isVisible
@@ -23,27 +25,45 @@ class MainActivity : AppCompatActivity() {
         val dice = Dice(6)
 
         button.setOnClickListener {
-            if (image.isInvisible) {
-                image.isVisible = true
+            if (image1.isInvisible) {
+                image1.isVisible = true
+                image2.isVisible = true
                 text.isVisible = false
             }
 
-            val side = dice.roll()
+            val side1 = image1.roll(dice, false)
+            val side2 = image2.roll(dice)
 
-            val id = when (side) {
-                1 -> R.drawable.dice_1
-                2 -> R.drawable.dice_2
-                3 -> R.drawable.dice_3
-                4 -> R.drawable.dice_4
-                5 -> R.drawable.dice_5
-                else -> R.drawable.dice_6
+            val msg = if (side1 == side2) {
+                "It's a tie!"
+            } else if (side1 > side2) {
+                "Dice 1 wins!"
+            } else {
+                "Dice 2 wins!"
             }
 
-            with(image) {
-                animate().rotationBy(360f).start()
-                contentDescription = getString(R.string.dice_side, side)
-                setImageResource(id)
-            }
+            Toast
+                .makeText(this@MainActivity, msg, Toast.LENGTH_SHORT)
+                .show()
         }
+    }
+
+    private fun ImageView.roll(dice: Dice, rotateClockwise: Boolean = true): Int {
+        val side = dice.roll()
+
+        val id = when (side) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        animate().rotationBy(if (rotateClockwise) 360f else -360f).start()
+        contentDescription = getString(R.string.dice_side, side)
+        setImageResource(id)
+
+        return side
     }
 }
